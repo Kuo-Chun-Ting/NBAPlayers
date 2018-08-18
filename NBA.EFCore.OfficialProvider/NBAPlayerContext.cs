0ec062e064d637ac4325774398d90477428e3eaf
Kuo-Chun-Ting <halila@me.com>
2018-08-19 02:18:06 +0800

    official mysql provider migration OK

---
+++ NBA.EFCore.OfficialProvider/NBAPlayerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBA.EFCore.OfficialProvider
+{
+    public class TestContext : DbContext
+    {
+        public DbSet<PlayerModel> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=test;user=root;password=lillard1611005");
+            //optionsBuilder.UseMySql("server=localhost;database=nba;uid=root;pwd=lillard1611005");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PlayerModel>(entity =>
+            {
+                entity.HasKey(p => p.id);
+                entity.Property(p => p.name).IsRequired();
+            });
+        }
+    }
+}
---
+++ NBA.EFCore.OfficialProvider/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NBA.EFCore.OfficialProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InsertData();
+                Console.WriteLine("Success!");
+            }
+            catch (Exception e)
+            {
+
+                WriteMsg(e.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        private static void InsertData()
+        {
+            using (var context = new TestContext())
+            {
+                // Creates the database if not exists
+                context.Database.EnsureCreated();
+
+                // Adds a publisher
+                var player = new PlayerModel
+                {
+                    id = Guid.NewGuid().ToString(),
+                    name = "test",
+                    team_name = "gws"
+                };
+                context.Players.Add(player);
+
+                // Saves changes
+                context.SaveChanges();
+            }
+        }
+
+        private static void WriteMsg(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
